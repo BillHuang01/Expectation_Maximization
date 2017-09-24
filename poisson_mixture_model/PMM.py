72c1f56157e9b603e1affe49c703927fca06c144
Bill Huang <billhuang@Bills-MacBook-Air.local>
2017-09-24 14:39:29 -0400

    possion mixture model

---
+++ poisson_mixture_model/PMM.py
@@ -0,0 +1,44 @@
+__author__ = 'billhuang'
+
+import numpy as np
+import numerical_utils as nu
+from scipy import stats
+
+def random_initialization(Y_, K_):
+    mean_ = np.mean(Y_)
+    pi_ = np.random.dirichlet(np.ones(K_))
+    lambda_ = np.random.gamma(1.0, mean_, size = K_)
+    return (pi_, lambda_)
+
+def E_step(Y_, pi_, lambda_):
+    N_ = Y_.size
+    K_ = lambda_.size
+    logZ_ = np.zeros((N_, K_))
+    logZ_ += nu.log(pi_)
+    for k in range(K_):
+        logZ_[:,k] += stats.poisson.logpmf(Y_, lambda_[k])
+    Z_ = nu.normalize_log_across_row(logZ_)
+    lower_bound_ = np.sum(Z_ * (logZ_ - nu.log(Z_)))
+    return (Z_, lower_bound_)
+
+def M_step(Y_, Z_):
+    N_, K_ = Z_.shape
+    Nk_ = np.sum(Z_, axis = 0)
+    pi_ = Nk_ / N_
+    lambda_ = np.dot(Z_.T, Y_) / Nk_
+    return (pi_, lambda_)
+
+def PMM(Y_, K_, eps = np.power(0.1, 3),
+        initializer = random_initialization):
+    pi_, lambda_ = initializer(Y_, K_)
+    lower_bound = np.array([])
+    continue_ = True
+    while (continue_):
+        Z_, lower_bound_ = E_step(Y_, pi_, lambda_)
+        lower_bound = np.append(lower_bound, lower_bound_)
+        pi_, lambda_ = M_step(Y_, Z_)
+        if (lower_bound.size > 1):
+            if ((np.exp(lower_bound[-1] - lower_bound[-2]) - 1) < eps):
+                continue_ = False
+    group_ = np.argmax(Z_, axis = 1)
+    return (group_)
---
+++ poisson_mixture_model/example.py
@@ -0,0 +1,25 @@
+__author__ = 'billhuang'
+
+import numpy as np
+import PMM
+
+# SET RANDOM SEED
+np.random.seed(1234)
+
+# CREATE DATA
+N = [10, 20, 30]
+
+lambda_ = np.array([5, 25, 45])
+
+Y = np.zeros(np.sum(N), dtype = int)
+for i in range(N[0]):
+    Y[i] = np.random.poisson(lambda_[0])
+for i in range(N[0], (N[0]+N[1])):
+    Y[i] = np.random.poisson(lambda_[1])
+for i in range((N[0]+N[1]), np.sum(N)):
+    Y[i] = np.random.poisson(lambda_[2])
+
+# INFERENCE BY EM
+label = PMM.PMM(Y, 3)
+print(label)
+
