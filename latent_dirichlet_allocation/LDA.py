a29547ad8a1b6e2c96b99cf266e61642eb9f84b1
Bill Huang <billhuang@dyn-129-236-214-246.dyn.columbia.edu>
2017-09-28 18:30:12 -0400

    latent dirichlet allocation

---
+++ latent_dirichlet_allocation/LDA.py
@@ -0,0 +1,58 @@
+__author__ = 'billhuang'
+
+import numpy as np
+import numerical_utils as nu
+
+def random_initialization(K_, D_, V_):
+    theta_ = nu.log(np.random.dirichlet(np.ones(K_), size = D_))
+    beta_ = nu.log(np.random.dirichlet(np.ones(V_), size = K_))
+    return (theta_, beta_)
+
+def update_theta(phi_, K_, D_):
+    count_ = np.zeros((D_, K_))
+    for d in range(D_):
+        count_[d,:] = np.sum(phi_[d], axis = 0)
+    theta_ = nu.log(nu.normalize(count_))
+    return (theta_)
+
+def update_beta(Y_, phi_, K_, D_, V_):
+    count_ = np.zeros((K_, V_))
+    for k in range(K_):
+        for d in range(D_):
+            for i in range(len(Y_[d])):
+                count_[k,Y_[d][i]] += phi_[d][i,k]
+    beta_ = nu.log(nu.normalize(count_))
+    return (beta_)
+
+def E_step(Y_, theta_, beta_, K_, D_):
+    lower_bound_ = 0
+    phi_ = []
+    for d in range(D_):
+        logphid = np.zeros((Y_[d].size, K_))
+        logphid += theta_[d,:]
+        for k in range(K_):
+            logphid[:,k] += beta_[k,:][Y_[d]]
+        phid_ = nu.normalize_log_across_row(logphid)
+        lower_bound_ += np.sum(phid_ * (logphid - nu.log(phid_)))
+        phi_.append(phid_)
+    return (phi_, lower_bound_)
+
+def M_step(Y_, phi_, K_, D_, V_):
+    theta_ = update_theta(phi_, K_, D_)
+    beta_ = update_beta(Y_, phi_, K_, D_, V_)
+    return (theta_, beta_)
+
+def LDA(Y_, K_, D_, V_, eps = np.power(0.1, 3),
+        initializer = random_initialization):
+    theta_, beta_ = initializer(K_, D_, V_)
+    lower_bound = np.array([])
+    continue_ = True
+    while (continue_):
+        phi_, lower_bound_ = E_step(Y_, theta_, beta_, K_, D_)
+        lower_bound = np.append(lower_bound, lower_bound_)
+        theta_, beta_ =M_step(Y_, phi_, K_, D_, V_)
+        if (lower_bound.size > 1):
+            if ((np.exp(lower_bound[-1] - lower_bound[-2]) - 1) < eps):
+                continue_ = False
+    return (theta_, beta_)
+    
