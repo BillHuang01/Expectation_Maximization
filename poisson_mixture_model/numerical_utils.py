1825d92e10ed9381a860e481cfaa07d2398a542c
Bill Huang <billhuang@Bills-MacBook-Air.local>
2017-09-27 22:25:54 -0400

    numerical utils update

--- poisson_mixture_model/numerical_utils.py
+++ poisson_mixture_model/numerical_utils.py
@@ -8,15 +8,13 @@ def log(x):
     '''
     return (np.nan_to_num(np.log(x)))
 
-def normalize_log_across_row(UN):
+def normalize_log_across_row(logM):
     '''
     safer way to convert log probability to normalized
     probability across row in a matrix than simply exponential
     each term and then normalize
     '''
-    # ULP: unnormalized_log_probability
-    # subtract each row by its max
-    MLP = np.transpose(np.transpose(UN) - np.max(UN, axis=1))
-    UP = np.exp(MLP)
-    N = np.transpose(np.transpose(UP)/np.sum(UP, axis=1))
-    return (N)
+    logP = np.logaddexp.reduce(logM, axis = 1)
+    NlogM = (logM.T - logP).T
+    M = np.exp(NlogM)
+    return (M)
