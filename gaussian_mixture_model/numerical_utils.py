1f0331e9c7914b20d702c345834a712a266b4652
Bill Huang <billhuang@Bills-MacBook-Air.local>
2017-09-27 22:23:56 -0400

    numerical utils update

--- gaussian_mixture_model/numerical_utils.py
+++ gaussian_mixture_model/numerical_utils.py
@@ -8,16 +8,6 @@ def log(x):
     '''
     return (np.nan_to_num(np.log(x)))
 
-'''
-def normalize_log_across_row(UN):
-    # ULP: unnormalized_log_probability
-    # subtract each row by its max
-    MLP = np.transpose(np.transpose(UN) - np.max(UN, axis=1))
-    UP = np.exp(MLP)
-    N = np.transpose(np.transpose(UP)/np.sum(UP, axis=1))
-    return (N)
-'''
-
 def normalize_log_across_row(logM):
     '''
     safer way to convert log probability to normalized
