3bdbd71787ed5ef4616009aa73277fc486304bb0
Bill Huang <billhuang@Bills-MacBook-Air.local>
2017-10-01 02:28:45 -0400

    output platform update

--- poisson_mixture_model/PMM.py
+++ poisson_mixture_model/PMM.py
@@ -1,9 +1,10 @@
+from __future__ import absolute_import, division, print_function
+
 __author__ = 'billhuang'
 
 import numpy as np
 import numerical_utils as nu
 from scipy import stats
-import sys
 
 def random_initialization(Y_, K_):
     mean_ = np.mean(Y_)
@@ -37,14 +38,14 @@ def PMM(Y_, K_, eps = np.power(0.1, 3),
     lower_bound = np.array([])
     continue_ = True
     while (continue_):
-        sys.stdout.write('*')
+        print('*', end = '')
         Z_, lower_bound_ = E_step(Y_, pi_, lambda_)
         lower_bound = np.append(lower_bound, lower_bound_)
         pi_, lambda_ = M_step(Y_, Z_)
         if (lower_bound.size > 1):
             if ((np.exp(lower_bound[-1] - lower_bound[-2]) - 1) < eps):
                 continue_ = False
-                sys.stdout.write('  done!\n')
+                print('  done!')
     print('pi')
     print(pi_)
     print('lambda')
